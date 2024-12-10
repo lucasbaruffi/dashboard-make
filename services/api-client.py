24b8e33bbf1f0d8f834434eccfb0ddbab779c37f
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-10 09:43:47 -0300

    morreu aqui
    tá dificil puxar as variáveis globais

--- services/api-client.py
+++ services/api-client.py
@@ -1,6 +1,13 @@
 # Chamada da API
+import sys
+from pathlib import Path
+
+# Adiciona o diretório pai ao sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from config.settings import url, apiVersion
 
 import requests
-from config.settings
+from ..config.settings import url, apiVersion
 
 print(url, apiVersion)
