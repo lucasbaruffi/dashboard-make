5393388a35a344e5c6b5f348ec0be68a14394c0d
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-13 08:51:26 -0300

    pegando as informações de cenarios

--- services/api-client.py
+++ services/api-client.py
@@ -1,12 +1,19 @@
 # Chamada da API
 from config import settings
 import endpoints
+import json
 
 url = settings.MAKE_URL
 token = settings.MAKE_TOKEN
 organizationId = settings.MAKE_ORGANIZATION_ID
 
-res = endpoints.scenarios(url, token, organizationId, 1)
-res = dict(res)
-for c in res["scenarios"]:
-    print(res["scenarios"]["id"])
+res = endpoints.scenarios(url, token, organizationId, 2)
+res = json.loads(res)
+
+
+for cenario in res["scenarios"]:
+    print(cenario["id"])
+
+
+#for c in res["scenarios"]:
+ #   print(res["scenarios"][c]["id"])
