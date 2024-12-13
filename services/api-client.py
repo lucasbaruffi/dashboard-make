01d14bac6a77a8ef67b8253cf3dac87485e84068
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-13 09:01:51 -0300

    pelo menos puxa infos
    próximo passo acho que é salvar os dados em algum lugar

--- services/api-client.py
+++ services/api-client.py
@@ -7,12 +7,18 @@ url = settings.MAKE_URL
 token = settings.MAKE_TOKEN
 organizationId = settings.MAKE_ORGANIZATION_ID
 
-res = endpoints.scenarios(url, token, organizationId, 2)
+res = endpoints.scenarios(url, token, organizationId, 1)
 res = json.loads(res)
 
 
 for cenario in res["scenarios"]:
-    print(cenario["id"])
+    print(cenario)
+    id = cenario["id"]
+    name = cenario["name"]
+   # folder = cenario["folder"]
+
+
+    print(f"{id} - {name}")
 
 
 #for c in res["scenarios"]:
